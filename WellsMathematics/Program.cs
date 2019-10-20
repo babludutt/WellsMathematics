ef159637bc3d60ce5f06da1273f171730f27e8d8
babludutt <babludutt@users.noreply.github.com>
2019-10-20 02:18:49 -0400

    Math.Pow function

---
+++ WellsMathematics/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+namespace WellsMathematics
+{
+    public class PowerProgram
+    {
+        public static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Enter x, y to calculate Power(x, y)");
+                try
+                {
+                    double.TryParse(Console.ReadLine(), out double x);
+
+                    double.TryParse(Console.ReadLine(), out double y);
+
+                    double result = Power(x, y);
+                    Console.WriteLine(result);
+
+                    //Console.WriteLine("Output using Math.Power = " + Math.Pow(x, y));
+                }
+                catch (OverflowException)
+                {
+                    Console.WriteLine("OverflowException occured, please enter smaller values");
+                }
+                Console.Read();
+            }
+
+        }
+
+        public static double Power(double x, double n)
+        {
+            double result = 1;
+            while (n > 0)
+            {
+                if (n % 2 > 0)
+                {
+                    result = result * x;
+                    n = n - 1;
+                }
+
+                x = x * x;
+                n = n / 2;
+            }
+
+            return result;
+        }
+    }
+}
